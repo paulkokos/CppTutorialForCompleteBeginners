4e3decadca60bdbe76f86b6f56b2002ba52883ba
paulkokos <paulkokos@yahoo.gr>
2020-12-05 19:27:43 +0200

    Initial commit

--- initial_creation.sh
+++ initial_creation.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/bash
 cd src || exit
-startFolder="$1"
-endFolder="$2"
-for n in $(seq -w $startFolder $endFolder); do
-    mkdir "[$n]"
-    cd "[$n]" || exit
-    echo "#include <iostream>
-int main() {
-    std::cout << \"Hello, World!\" << std::endl;
-    return 0;
-}" > main.cpp
-
-    touch readme.md
-
-    touch CMakeLists.txt
-    cd ..
+sections="$1"
+#startFolder="$2"
+#endFolder="$3"
+for n in $(seq -w $sections); do
+  mkdir "Section[$n] : "
+  cd "Section[$n] : " || exit
+  touch readme.md
+  touch CMakeLists.txt
+  cd ..
 done
+#for n in $(seq -w $startFolder $endFolder); do
+#    mkdir "[$n]"
+#    cd "[$n]" || exit
+#
+#    echo "#include <iostream>
+#int main() {
+#    std::cout << \"Hello, World!\" << std::endl;
+#    return 0;
+#}" > main.cpp
+#
+#    touch readme.md
+#
+#    touch CMakeLists.txt
+#    cd ..
+#done
